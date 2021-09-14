d12e958a81e57fe828364864f74e183c76606ab5
masatakamaekawa <mstkmekw@hito-koto.com>
2021-09-14 15:20:28 +0900

    テスト1

---
+++ resources/views/greeting/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>あいさつ</title>
+</head>
+
+<body>
+    <h1>朝のあいさつ</h1>
+    <h2>{{ $message }}</h2>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Psr7\Message;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('comments/{message}', [App\Http\Controllers\HelloController::class, 'hello']);
